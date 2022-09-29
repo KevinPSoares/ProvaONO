72c8f5eb04612b753fc6b7c382b071ece8d50571
Kevin <88691834+KevinPSoares@users.noreply.github.com>
2022-09-29 14:55:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -220,3 +220,23 @@ const $checkbox = document.querySelector('#switch')
 $checkbox .addEventListener('change', function(){
     $html.classList.toggle('dark-mode')
 })
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
